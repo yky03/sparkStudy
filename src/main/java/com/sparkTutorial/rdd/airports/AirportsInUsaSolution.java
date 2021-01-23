90ac7d1f36039080c0f09294cb7a903e22e4ac3c
양기열 <ykycome00@naver.com>
2021-01-23 10:50:42 +0900

    initial commit

---
+++ src/main/java/com/sparkTutorial/rdd/airports/AirportsInUsaSolution.java
@@ -0,0 +1,37 @@
+package com.sparkTutorial.rdd.airports;
+
+import com.sparkTutorial.rdd.commons.Utils;
+import org.apache.commons.lang.StringUtils;
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+
+import java.util.Scanner;
+
+public class AirportsInUsaSolution {
+
+    public static void main(String[] args) throws Exception {
+
+        SparkConf conf = new SparkConf().setAppName("airports").setMaster("local[2]");
+
+        JavaSparkContext sc = new JavaSparkContext(conf);
+
+        JavaRDD<String> airports = sc.textFile("in/airports.text");
+
+        JavaRDD<String> airportsInUSA = airports.filter(line -> line.split(Utils.COMMA_DELIMITER)[3].equals("\"United States\""));
+
+        JavaRDD<String> airportsNameAndCityNames = airportsInUSA.map(line -> {
+                    String[] splits = line.split(Utils.COMMA_DELIMITER);
+                    return StringUtils.join(new String[]{splits[1], splits[2]}, ",");
+                }
+        );
+
+        //http://localhost:4040/storage/
+        Scanner scanner = new Scanner(System.in);
+        scanner.nextLine();
+
+        scanner.close();
+
+        airportsNameAndCityNames.saveAsTextFile("out/airports_in_usa.text");
+    }
+}
